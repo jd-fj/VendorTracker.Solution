19a0ffe2b159f3abe8eedefc2381f1858f06e0b9
taylulz <taylulzcode@gmail.com>
2020-12-18 09:34:17 -0800

    add GetAll test

--- VendorTracker.Tests/ModelTests/OrderTests.cs
+++ VendorTracker.Tests/ModelTests/OrderTests.cs
@@ -15,8 +15,25 @@ namespace VendorTracker.Tests
       string newOrderDescription = "Lindas b&b Description";
       string newOrderDate = "12-18-20";
       int newOrderPrice = 100;
+
       Order newOrder = new Order(newOrderTitle, newOrderDescription, newOrderDate, newOrderPrice);
+
       Assert.AreEqual(newOrderTitle, newOrder.Title);
+    }
+
+    [TestMethod]
+    public void GetAll_ReturnsListOfInstantiatedOrderObjs_OrderInstances()
+    {
+      string newOrderTitle = "Lindas b&b";
+      string newOrderDescription = "Lindas b&b Description";
+      string newOrderDate = "12-18-20";
+      int newOrderPrice = 100;
+      Order newOrder = new Order(newOrderTitle, newOrderDescription, newOrderDate, newOrderPrice);
+      List<Order> newList = new List<Order> { newOrder };
+
+      List<Order> getResults = Order.GetAll();
+
+      CollectionAssert.AreEqual(newList, getResults);
     }
   }
 }
