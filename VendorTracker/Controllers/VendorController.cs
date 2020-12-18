affb651c4bc0f99b8779740cad5a4be7bc7ea230
taylulz <taylulzcode@gmail.com>
2020-12-18 15:47:34 -0800

    change vendor pathway to plural

--- VendorTracker/Controllers/VendorController.cs
+++ VendorTracker/Controllers/VendorController.cs
@@ -32,7 +32,7 @@ namespace VendorTracker.Controllers
       Dictionary<string, object> model = new Dictionary<string, object> ();
       Vendor selectedVendor = Vendor.Find(id);
       List<Order> vendorOrders = selectedVendor.Orders;
-      model.Add("vendor", selectedVendor);
+      model.Add("vendors", selectedVendor);
       model.Add("orders", vendorOrders);
       return View(model);
     }
