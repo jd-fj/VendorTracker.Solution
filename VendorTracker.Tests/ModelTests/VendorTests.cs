629169e6ad3401b44f35c082c22520df69f421c7
taylulz <taylulzcode@gmail.com>
2020-12-18 11:50:57 -0800

    write test for a GetAll() Vendor method

--- VendorTracker.Tests/ModelTests/VendorTests.cs
+++ VendorTracker.Tests/ModelTests/VendorTests.cs
@@ -1,6 +1,7 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using VendorTracker.Models;
 using System;
+using System.Collections.Generic;
 
 
 namespace VendorTracker.Tests
@@ -23,6 +24,15 @@ namespace VendorTracker.Tests
       string vendorDescription = "vendor description";
       Vendor newVendor = new Vendor(vendorName, vendorDescription);
       Assert.AreEqual(vendorName, newVendor.Name);
+    }
+    [TestMethod]
+    public void GetVendors_ReturnsAllVendorObjs_VendorInstance()
+    {
+      Vendor newVendor = new Vendor("vendorName", "vendorDescription");
+      Vendor newVendor2 = new Vendor("vendorName2", "vendorDescription2");
+      List<Vendor> newList = new List<Vendor> { newVendor, newVendor2 };
+      List<Vendor> getResults = Vendor.GetAll();
+      CollectionAssert.AreEqual(newList, getResults);
     }
   }
 }
