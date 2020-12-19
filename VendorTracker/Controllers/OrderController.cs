0f2bd35138475c79f0af7a59c1a04a04bbeb2c1b
taylulz <taylulzcode@gmail.com>
2020-12-19 09:51:00 -0800

    Make a New() route for Orders

--- VendorTracker/Controllers/OrderController.cs
+++ VendorTracker/Controllers/OrderController.cs
@@ -18,5 +18,10 @@ namespace VendorTracker.Controllers
     }
 
     [HttpGet("/vendors/{vendorId}/orders/new")]
+    public ActionResult New(int vendorId)
+    {
+      Vendor vendor = Vendor.Find(vendorId);
+      return View(vendor);
+    }
   }
 }
